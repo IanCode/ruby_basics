1971d18f1ab0d0455f482d7d4b03a5a4af8c8528
Ian <iwwhite@pugetsound.edu>
2017-10-02 15:10:53 -0700

    got basic views and database functionality working

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -53,4 +53,15 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+    post 'book/create'
+    get 'book/list'
+    get 'book/new'
+    get 'book/create'
+    get 'book/update'
+    get 'book/list'
+    get 'book/show'
+    get 'book/edit'
+    get 'book/delete'
+    get 'book/update'
+    get 'book/show_subjects'
 end
---
+++ db/migrate/20170920092526_legion_dev.rb
@@ -0,0 +1,4 @@
+class LegionDev < ActiveRecord::Migration
+  def change
+  end
+end
